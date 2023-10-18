f216e082b1cf0f9aab3ca1295f51efe240d3c4c9
devinnow8 <devinnow8@gmail.com>
2023-10-18 17:36:26 +0530

    fix: commented code removed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,8 @@ import { useNavigate } from "react-router-dom";
 import App from "../App";
 
 function Router() {
-  const navigate = useNavigate();
   return (
     <div>
-      {/* <button onClick={() => navigate(-1)}>Go back</button> */}
       <Routes>
         <Route  path="/" element={<App />} />
       </Routes>
